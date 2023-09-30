078a831c63aae2b8c7f6de6b590f5c03e0b5e71d
denniskimotho <denniskimotho222@gmail.com>
2023-09-30 08:52:46 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+   let customerNameUpperCase = customerName
+
+   return customerNameUpperCase.toUpperCase()
+}
+
+upperCaseCustomerName();
+ 
+
+function setBestCustomer() {
+    let setBestCustomer = customerName
+}
+
